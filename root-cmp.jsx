506461e701ee6dc3d4193f552a672b997ea8c459
May Almog <mayalmog@gmail.com>
2021-08-25 10:27:14 +0300

    root-cmp

---
+++ root-cmp.jsx
@@ -0,0 +1,31 @@
+// const Router = ReactRouterDOM.HashRouter
+// const { Route, Switch } = ReactRouterDOM
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+import { BookApp } from './pages/BookApp.jsx'
+import { Home } from './pages/Home.jsx'
+import { About } from './pages/About.jsx'
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { BookDetails } from './pages/BookDetails.jsx'
+
+// Simple React Component
+export function App() {
+    return (
+        <Router>
+            <header>
+                <AppHeader />
+            </header>
+            <main className="app">
+                <Switch>
+
+                    <Route path="/book/:bookId" component={BookDetails} />
+                    <Route path="/book" component={BookApp} />
+                    <Route path="/about" component={About} />
+                    <Route path="/" component={Home} />
+
+                </Switch>
+            </main>
+        </Router>
+    )
+}
+
