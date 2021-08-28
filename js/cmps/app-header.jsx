ba287c6dad083abaf0eae3c803f2dd9b749b8020
May Almog <mayalmog@gmail.com>
2021-08-28 22:43:29 +0300

    initial css header and maiz-main

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -1,35 +1,39 @@
 const { Link, NavLink, withRouter } = ReactRouterDOM
 
-export function _AppHeader() {
-    return (
-        <section className="app-header flex space-between align-center">
-            <div className="logo-and-user flex space-between align-center">
-                <Link to="/">
-                    <div className="header-logo">
-                    <img src="assets/img/corn4.png"/>
-                        Maíz
+export class AppHeader extends React.Component {
+
+    render() {
+
+        return (
+            <section className="app-header flex space-between align-center">
+                <div className="logo-and-user flex space-between align-center">
+                    <Link to="/">
+                        <div className="header-logo">
+                            <img src="assets/img/corn4.png" />
+                            Maíz
                         </div>
-                </Link>
+                    </Link>
 
-                <div className="user-logo">
-                    <span className="nav-icon fas fa-user"></span>
+                    <div className="user-logo">
+                        <span className="nav-icon fas fa-user"></span>
+                    </div>
                 </div>
-            </div>
 
-            <nav className="header-nav flex">
-                <NavLink exact to="/"><span className="nav-icon fas fa-home"></span></NavLink>
-                <NavLink to="/keeper"><span className="nav-icon fas fa-sticky-note"></span></NavLink>
-                <NavLink to="/zmail"><span className="nav-icon fas fa-envelope"></span></NavLink>
-                <NavLink to="/mbooks"><span className="nav-icon fas fa-book-open"></span></NavLink>
+                <nav className="header-nav flex">
+                    <NavLink exact to="/"><span className="nav-icon fas fa-home"></span></NavLink>
+                    <NavLink to="/keeper"><span className="nav-icon fas fa-sticky-note"></span></NavLink>
+                    <NavLink to="/zmail"><span className="nav-icon fas fa-envelope"></span></NavLink>
+                    <NavLink to="/mbooks"><span className="nav-icon fas fa-book-open"></span></NavLink>
 
+                </nav>
                 <Link to="/">
                     <div className="header-grid"><span className="nav-icon fas fa-th"></span></div>
                 </Link>
-            </nav>
 
 
-        </section>
-    )
+            </section>
+        )
+    }
 }
 
-export const AppHeader = withRouter(_AppHeader)
+// export const AppHeader = withRouter(_AppHeader)
